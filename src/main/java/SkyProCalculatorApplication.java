c3ee6cf74b9875c5ed6f99337e970fbd48602771
Evllooew <evllooev@icloud.com>
2023-09-10 10:57:17 +0300

    1

---
+++ src/main/java/SkyProCalculatorApplication.java
@@ -0,0 +1,13 @@
+package pro.Sky.Skyprocalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyProCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyProCalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/Sky/Skyprocalculator/SkyProCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.Sky.Skyprocalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyProCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
